34efc33e1fa39343c0950c14a88fabb5e61068dd
griffint <griffin.tschurwald@students.olin.edu>
2015-10-22 00:25:18 -0400

    adding photo gallery boilerplate

---
+++ design_development.php
@@ -0,0 +1,43 @@
+<?php include 'header.php';
+?>
+    <div>
+      <div class="body">Our design development page is here</div>
+      <ul class="row">
+        <li class="col-lg-2 col-md-2 col-sm-3 col-xs-4">
+          <img class="img-responsive" src="1.jpg">
+        </li>
+        <li class="col-lg-2 col-md-2 col-sm-3 col-xs-4">
+          <img class="img-responsive" src="2.jpg">
+        </li>
+        <li class="col-lg-2 col-md-2 col-sm-3 col-xs-4">
+          <img class="img-responsive" src="3.jpg">
+        </li>
+        <li class="col-lg-2 col-md-2 col-sm-3 col-xs-4">
+          <img class="img-responsive" src="4.jpg">
+        </li>
+        <li class="col-lg-2 col-md-2 col-sm-3 col-xs-4">
+          <img class="img-responsive" src="5.jpg">
+        </li>
+        <li class="col-lg-2 col-md-2 col-sm-3 col-xs-4">
+          <img class="img-responsive" src="6.jpg">
+        </li>
+      </ul>
+
+      <div class="modal fade" id="my<a href="http://www.jqueryscript.net/tags.php?/Modal/">Modal</a>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+
+        <div class="modal-dialog">
+
+          <div class="modal-content">
+
+            <div class="modal-body"> </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+  </body>
+</html>
+
