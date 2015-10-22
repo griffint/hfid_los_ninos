3af69ad61eafafaf2442dabd352ec88bf8f1f632
griffint <griffin.tschurwald@students.olin.edu>
2015-10-21 23:51:54 -0400

    switchin to nice php header

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/website.css" rel="stylesheet">
+	</head>
+	<body>
+		<nav class="navbar navbar-default navbar-static-top">
+			<div class = "container-fluid"> 
+				<a class="navbar-brand" href="#">Los Ninos</a>
+			</div>
+			<div>
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="index.php">Home</a></li>
+					<li><a href="projectbrief.php">Project Brief</a></li>
+					<li><a href="idesigns.php">Inspirational Designs</a></li>
+					<li><a href="needsanalysis.php">Needs Analysis</a></li> 
+					<li><a href="work_distribution.php">Work Distribution</a></li>
+			      </ul>
+			 </div>
+		</nav>
