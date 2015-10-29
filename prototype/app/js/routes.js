1bf955f358f87f21fe0e7c754de1aef2c870d3a3
jaazpa3 <jaazpa3@gmail.com>
2015-10-29 16:21:31 -0400

    adding files

---
+++ prototype/app/js/routes.js
@@ -0,0 +1,25 @@
+// routes.js
+
+app.config(function ($stateProvider) {
+
+  $stateProvider
+    .state('welcome', {
+      url: '/welcome',
+      // ...
+      data: {
+        requireLogin: false
+      }
+    })
+    .state('app', {
+      abstract: true,
+      // ...
+      data: {
+        requireLogin: true // this property will apply to all children of 'app'
+      }
+    })
+    .state('app.dashboard', {
+      // child state of `app`
+      // requireLogin === true
+    })
+
+});
