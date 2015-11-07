61808f838eb8b32f8c15bf4c5489dbf49ffbdf3f
jaazpa3 <jaazpa3@gmail.com>
2015-11-06 22:02:10 -0500

    adding my changes

--- prototype/app/controllers/mapcontrollers.js
+++ prototype/app/controllers/mapcontrollers.js
@@ -7,6 +7,34 @@ app.controller('mapSidebarCtrl', ["$scope", "$firebaseArray",
 		console.log($scope.cards[0]);
 	}*/
 	function($scope){
-		$scope.cards = [{ id: 0, name: "Timothy"}, {id: 1, name: "Jill"}];
+		$scope.cards = [{ id: 0, name: "Timothy", age: "22", title: "Chef", gender: "M"}, {id: 1, name: "Jill", title: "Writer", age: "23", gender: "F"}];
 	}
 ]);
+
+app.controller('ModalCtrl', function($scope, $uibModal, $log) {
+	$scope.animationsEnabled = true;
+
+	$scope.open = function (size) {
+		$scope.cards = $scope.cards;
+
+	    var modalInstance = $uibModal.open({
+	      animation: $scope.animationsEnabled,
+	      templateUrl: 'profiledetails.html',
+	      controller: 'ModalInstanceCtrl',
+	      size: size,
+	    });
+	}
+});
+
+app.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance) {
+
+  $scope.ok = function () {
+    //$uibModalInstance.close($scope.selected.item);
+    //closes the tab
+    $uibModalInstance.dismiss('cancel');
+  };
+
+  $scope.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
