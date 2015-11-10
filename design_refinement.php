08d787b683e058ea710e93ebdad0ae6a40e30a9e
griffint <griffin.tschurwald@students.olin.edu>
2015-11-09 22:01:54 -0500

    adding some stuff for design refinement

--- design_refinement.php
+++ design_refinement.php
@@ -4,10 +4,13 @@
     <div class="body">Our design development page is here</div>
     	<h1>Design Refinement</h1>
     	<h4>The button below will take you to our working prototype. Information about this assignment can be found below</h4>
-    	<button href="prototype/index.html">Prototype</button>
+    	<a href="/prototype/index.html">Prototype</a>
+        <br>
+        <h3>Prototype testing instructions</h3>
     	<br>
     	<h5>Wireframes</h5>
     	<p>Here we'll put some information about our wireframes</p>
+
     </div>
   </body>
   <script src="photo-gallery.js"></script>
