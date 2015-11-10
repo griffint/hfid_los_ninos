72ad0def4f2e5cacb810ff8f89265cd55f8f1843
griffint <griffin.tschurwald@students.olin.edu>
2015-11-09 22:37:37 -0500

    adding some stuff for design refinement

--- design_refinement.php
+++ design_refinement.php
@@ -1,12 +1,29 @@
 <?php include 'header.php';
 ?>
-    
+    <style>
+    p
+    {
+      font-size: 1em;
+    }
+    </style>
     <div class="body">Our design development page is here</div>
     	<h1>Design Refinement</h1>
     	<h4>The link below will take you to our working prototype. Information about this assignment can be found below</h4>
     	<a href="prototype/index.html">Prototype</a>
         <br>
         <h3>Prototype testing instructions</h3>
+        <br>
+        <p>Our prototype is a functioning html site with a not fully functional backend. That is, you can test most interactions but your data will not be 
+        persistently saved. Below we've laid out some tasks for you to try that our personas would have to do in their daily usage of our site. 
+        </p>
+        <h5>Task 1</h5>
+        <p>Create an account by registering with the site, and fill out your profile with some basic and advanced information.</p>
+        <h5>Task 2</h5>
+        <p>Go to the map view and look through the people and places that are available. Use filters to narrow your search, and add someone to your bookmarks.</p>
+        <h5>Task 3</h5>
+        <p>Go check out your bookmaks, look at more info to find out more about them.</p>
+        <h5>Task 4</h5>
+        <p>Look at your pending group requests, and accept or reject whatever one seems better</p>
     	<br>
     	<h5>Wireframes</h5>
     	<p>Here we'll put some information about our wireframes</p>
