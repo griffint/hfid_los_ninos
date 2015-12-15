c35b6479b3688618c1def827e675c41b2da2c6c6
griffin tschurwald <griffintschurwald@griffins-mbp.olin.edu>
2015-12-15 16:53:45 -0500

    final writeup page

--- final_writeup.php
+++ final_writeup.php
@@ -1,7 +1,50 @@
 <?php include 'header.php';
 ?>
-<div class="body">
-<!--stuff goes here-->
-</div>
-</body>
+    <style>
+    p
+    {
+      font-size: 1em;
+    }
+    img
+    { 
+      display:block;
+      text-align: center;
+      margin: 0 auto;
+      width: 400px;
+    }
+    div.image:hover img.jasperimages
+    {
+      display: block;
+      text-align: center;
+      margin: 0 auto;
+      width: 900px;
+    }
+    
+    .caption{
+      text-align: center;
+      margin: 0 auto;
+    }
+    </style>
+      <?php
+        function images($dir) {
+        $files1 = scandir($dir);
+        foreach ($files1 as $x=>$value)
+        {
+            if ($value == "." || $value == ".."){
+                unset($files1[$x]);
+            }
+        }
+        foreach ($files1 as $x=>$value)
+        {
+
+            echo "<li class=\"col-md-6\"><img class=\"img-responsive\" src=\"$dir$value\"></li>";
+        }
+        }
+        images("imgs/wireframes/");
+      ?>
+      </ul>
+
+    </div>
+  </body>
+  <script src="photo-gallery.js"></script>
 </html>
